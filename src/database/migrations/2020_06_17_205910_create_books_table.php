bba926ae08c0064766429df835c1700bc31e52f8
horenko_vp <vladzpru99@gmail.com>
2020-06-18 03:51:09 +0300

    add models/migrations add auth-system: controller/routes/middlewares/configs; jwt-based change README: add setup-steps remove helloworld-routes from web-routes

---
+++ src/database/migrations/2020_06_17_205910_create_books_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+
+            $table->string('title');
+
+            $table->integer('pages')->unsigned();
+            $table->text('annotation')->nullable();
+
+            $table->string('image')->comment('in base_64 format');
+
+            $table->unsignedBigInteger('author_id');
+            $table->foreign('author_id')->references('id')->on('authors');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
